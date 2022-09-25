efbe47bca9dc4b6c80f53bef4a30ee79705c23e9
Nunder <nathannunderwood@gmail.com>
2022-09-24 22:50:57 -0700

    Update schema tables and start initial prompt build server.js

--- schema.sql
+++ schema.sql
@@ -5,22 +5,23 @@ CREATE DATABASE employeeTracker_db;
 USE employeeTracker_db;
 
 CREATE TABLE department (
-    department_id INT AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30)
+    name VARCHAR(30),
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
 );
 
 CREATE TABLE role (
-    role_id INT AUTO_INCREMENT PRIMARY KEY,
-    role_title VARCHAR(50), 
-    salary INT, 
-    department_id INT, 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL, 
+    department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(department_id)  ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    employee_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT, 
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(role_id) ON DELETE CASCADE,
--- server.js
+++ server.js
@@ -5,10 +5,53 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: process.env.password,
-    database: 'employeeTrackerDB',
-});
+const db = mysql.createConnection(
+    {
+      host: process.env.host,
+      // MySQL username,
+      user: process.env.user,
+      // MySQL password
+      password: process.env.password,
+      database: 'employeeTracker_db'
+    },
+    console.log(`Connected to the employeeTracker_db database.`);
+    employeeTime();
+  );
+
+  employeeTime = () => {
+    inquirer.prompt([
+        {
+            name: 'firstQuestion', 
+            type: 'list', 
+            message: 'Thank you for choosing Employee Tracker. Where should we start?',  
+            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role, add an employee', 'update an employee role']
+        }
+    ]).then(response) => {
+        switch(response.firstQuestion) {
+            case 'view all departments': viewAllDepartments(); break;
+
+            case 'view all roles': viewAllRoles();
+            break;
+
+            case 'view all employees': viewAllEmployees(); 
+            break;
+
+            case 'add a department': addDepartment(); 
+            break;
+
+            case 'add a role': 
+            addRole();
+            break;
+
+            case 'add an employee':
+            addEmployee();
+            break;
+
+            case 'update employee role': 
+            updateEmployeeRole();
+            break;
+        }
+  }
+
+
+  
