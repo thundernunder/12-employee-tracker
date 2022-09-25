75d280433638d213584a7293dd5957906731ab6e
Nunder <nathannunderwood@gmail.com>
2022-09-24 21:08:47 -0700

    Build tables for schema

--- schema.sql
+++ schema.sql
@@ -8,3 +8,22 @@ CREATE TABLE department (
     department_id INT AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30)
 );
+
+CREATE TABLE role (
+    role_id INT AUTO_INCREMENT PRIMARY KEY,
+    role_title VARCHAR(50), 
+    salary INT, 
+    department_id INT, 
+    FOREIGN KEY (department_id) REFERENCES department(department_id)  ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    role_id INT, 
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(role_id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(employee_id) ON DELETE SET NULL
+
+);
