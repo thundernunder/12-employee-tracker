7e37f75629dad9fe98875850eabf473bf8558faa
Nunder <nathannunderwood@gmail.com>
2022-09-24 23:00:13 -0700

    Fix syntax error server.js

--- server.js
+++ server.js
@@ -14,44 +14,44 @@ const db = mysql.createConnection(
       password: process.env.password,
       database: 'employeeTracker_db'
     },
-    console.log(`Connected to the employeeTracker_db database.`);
-    employeeTime();
-  );
+    console.log(`Connected to the employeeTracker_db database.`),
+    employeeTime()
+);
 
-  employeeTime = () => {
-    inquirer.prompt([
-        {
-            name: 'firstQuestion', 
-            type: 'list', 
-            message: 'Thank you for choosing Employee Tracker. Where should we start?',  
-            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role, add an employee', 'update an employee role']
-        }
-    ]).then(response) => {
-        switch(response.firstQuestion) {
-            case 'view all departments': viewAllDepartments(); break;
+employeeTime = () => {
+inquirer.prompt([
+    {
+        name: 'firstQuestion', 
+        type: 'list', 
+        message: 'Thank you for choosing Employee Tracker. Where should we start?',  
+        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role, add an employee', 'update an employee role']
+    }
+]).then((response) => {
+    switch(response.firstQuestion) {
+        case 'view all departments': viewAllDepartments(); break;
 
-            case 'view all roles': viewAllRoles();
-            break;
+        case 'view all roles': viewAllRoles();
+        break;
 
-            case 'view all employees': viewAllEmployees(); 
-            break;
+        case 'view all employees': viewAllEmployees(); 
+        break;
 
-            case 'add a department': addDepartment(); 
-            break;
+        case 'add a department': addDepartment(); 
+        break;
 
-            case 'add a role': 
-            addRole();
-            break;
+        case 'add a role': 
+        addRole();
+        break;
 
-            case 'add an employee':
-            addEmployee();
-            break;
+        case 'add an employee':
+        addEmployee();
+        break;
 
-            case 'update employee role': 
-            updateEmployeeRole();
-            break;
-        }
-  }
+        case 'update employee role': 
+        updateEmployeeRole();
+        break;
+    }});
+}
 
 
   
